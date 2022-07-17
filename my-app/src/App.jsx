a6bbc56f94d3304913e2cbc16fde062682e463e6
Miljan Cosovic <miljancosovicnp@gmail.com>
2022-07-17 09:51:45 +0200

    React app

---
+++ my-app/src/App.jsx
@@ -0,0 +1,32 @@
+import "./App.css";
+import Card from "./Card"
+
+
+
+
+
+
+
+const App = () =>{
+  
+  return <div className="container">
+    <h1>Todo List</h1>
+    <div className="unos">
+      <input type="text" />
+      <button>Submit</button>
+    </div>
+    
+    <Card />
+    <Card />
+
+    
+
+
+
+
+    
+   
+  </div>
+}
+
+export default App;
---
+++ my-app/src/Card.jsx
@@ -0,0 +1,13 @@
+const Card = () =>{
+    return <div>
+         <div className="card">
+      <p>Nesto</p>
+      <div>
+        <button className="check">check</button>
+        <button className="delete">delete</button>
+      </div>
+    </div>
+    </div>
+}
+
+export default Card
