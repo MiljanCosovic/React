0eb594ef22c29cab200b0020096e6af1faefa0e2
Miljan Cosovic <miljancosovicnp@gmail.com>
2022-07-30 09:13:48 +0200

    React

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,17 +1,19 @@
 import "./App.css"
-import Form from "./components/Form"
+// import Calculator from "./components/Calculator"
+// import Form from "./components/Form"
 // import { useEffect, useState } from "react";
 // import Fetch from "./components/Fetch";
 // import DataFetching from "./components/DataFetching"
+import Fetch from "./components/Fetch"
 
 
 
 const App = () =>{
  return(
- <div>
+ <div className=".container">
   {
-  
-<Form />
+   <Fetch />
+
  
   
   }
